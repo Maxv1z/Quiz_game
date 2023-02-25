a30fe91fed3091ec5e9e04801f562618ef80cb39
Maxv1z <meitstudying@gmail.com>
2023-02-25 11:26:19 +0200

    Adding files

---
+++ script.js
@@ -0,0 +1,42 @@
+var skip = document.getElementById('skip');
+var score = document.getElementById('score');
+var totalScore = document.getElementById('totalScore');
+var countdown = document.getElementById('countdown');
+var count = 0;
+var scoreCount = 0;
+
+var duration = 0;
+
+var qaSets = document.querySelectorAll('.qa_set');
+
+qaSets.forEach(function(qaSet) {
+  var qaAnsRow = qaSet.querySelectorAll('.qa_ans_row input');
+  var selectedAns = null;
+  
+  qaAnsRow.forEach(function(qaAnsRowSingle) {
+    qaAnsRowSingle.addEventListener('click', function(){
+      if (!selectedAns) {
+        selectedAns = this;
+        
+        let valid = this.getAttribute("valid");
+
+        if (valid == "valid"){
+          scoreCount += 20;
+          score.innerHTML = scoreCount;
+          totalScore.innerHTML = scoreCount;
+        }
+        else{
+          scoreCount += 0;
+          score.innerHTML = scoreCount;
+          totalScore.innerHtml = scoreCount;
+        }
+        
+        qaAnsRow.forEach(function(qaAnsRowSingle) {
+          if (qaAnsRowSingle !== selectedAns) {
+            qaAnsRowSingle.disabled = true;
+          }
+        });
+      }
+    });
+  });
+});
